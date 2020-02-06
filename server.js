ef460b0622d6bb7ac7e938e1fe07fac6cee36e35
hp <nanitamo19@gmail.com>
2020-02-06 17:30:22 +0100

    Setup Inicial

---
+++ server.js
@@ -0,0 +1,45 @@
+if(process.env.NODE_ENV !== 'production'){
+  require('dotenv')
+}
+
+const expressLayouts = require('express-ejs-layouts')
+const express = require('express')
+const mongoose = require('mongoose')
+const MongoClient = require('mongodb').MongoClient;
+
+const app = express();
+
+// mongoose.connect(process.env.DATABASE_URL, {
+//   useNewUrlParser: true
+// })
+// const db = mongoose.connection
+// db.on('error', error => console.error(error))
+// db.once('open', open =>console.log('Connectado ao MongoDB'))
+
+
+const uri = "mongodb+srv://nanitamo:nanitamo@cluster0-rmanb.mongodb.net/livraria?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+client.connect(err => {
+  const collection = client.db("livraria").collection("livros")
+  collection.insertOne({
+    name: "Nanitamo",
+    age: "28",
+    deggre: "licenciatura"
+  })
+  client.close()
+})
+
+const indexRouter = require('./routes/index')
+
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
+app.set('layout', 'layouts/layout')
+app.use(expressLayouts)
+app.use(express.static('public'))
+
+app.use('/',indexRouter)
+
+app.listen(process.env.PORT || 3000)
+
+
+
